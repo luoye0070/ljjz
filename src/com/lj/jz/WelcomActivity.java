301a79389633f5eaef37efde4b0154ce175fdc15
luoye_lj <luoye.lj@sohu.com>
2014-10-18 20:57:13 +0800

    添加版本信息文件

--- src/com/lj/jz/WelcomActivity.java
+++ src/com/lj/jz/WelcomActivity.java
@@ -44,7 +44,8 @@ public class WelcomActivity extends Activity {
 		StaticParams.URL_VERSIONINFO="http://121.40.148.140/ljjz/version.txt";//版本信息访问地址
 		StaticParams.URL_UPDATEFILEPATH="http://121.40.148.140/ljjz/ljjz.apk";//app下载地址
 		StaticParams.UPDATE_SOFTWARE_FILE_NAME="ljjz.apk";//下载的最新app的保存的文件名
-		StaticParams.UPDATE_SOFTWARE_DIR_NAME="ljjzV/";//下载的最新的app的保存目录		
+		StaticParams.UPDATE_SOFTWARE_DIR_NAME="ljjzV/";//下载的最新的app的保存目录
+		StaticParams.appMsgTitle="灵久AA记账";
 		Intent intent=new Intent();
 		//intent.setClass(getApplicationContext(), VersionService.class);
 		intent.setClass(getApplicationContext(), VersionServiceNew.class);//新版本
